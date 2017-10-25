d6638f38fdcfdac87e13d920744b83bb07e5add9
Lubna Mirza <lmirza3@uic.edu>
2017-10-25 03:34:00 -0500

    test

---
+++ Andy Solar System Test/Assets/buttonH.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonH : MonoBehaviour {
+
+    public bool p;
+    public bool s;
+    public bool o;
+    public Renderer rend;
+      
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "VRController")
+        {
+            if (p == true)
+            {
+                rend.material.color = Color.red;
+            }
+            else if (s == true)
+            {
+                rend.material.color = Color.blue;
+            }
+            else if (o == true)
+            {
+                rend.material.color = Color.red;
+            }
+        }
+
+    }
+    // Use this for initialization
+    void Start () {
+
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
